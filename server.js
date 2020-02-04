e9338d640ee8f6a4d1c70f1bbfb8e4829dfaf504
Joey <joeyrodrigues92@gmail.com>
2020-02-04 10:22:46 -0500

    edited catchall handler in server.js

--- server.js
+++ server.js
@@ -69,10 +69,13 @@ app.post('/email', (req, res) =>{
     .catch(console.error);
 }); 
 
-
-app.use((req, res) =>
-  res.sendFile(path.join(__dirname, "../client/build/index.html"))
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get((req, res) =>
+  res.sendFile(path.join(__dirname, "./client/build/index.html"))
 );
+
+
 
 
 //console.log that your server is up and running
